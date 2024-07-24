48b9237cd585569e3327c545ae2532198c767b50
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-07-24 16:54:13 -0300

    test(): Setup Cypress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {AsyncPipe, JsonPipe} from "@angular/common";
+import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, AsyncPipe],
+  imports: [RouterOutlet, JsonPipe, AsyncPipe, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
