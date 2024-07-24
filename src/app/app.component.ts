950b15fe0be9854df4cf42abc882af0c95455175
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-07-24 16:35:42 -0300

    feat: Gets user from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {AsyncPipe, JsonPipe} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-miragejs-app';
+  user$: Observable<any>;
+
+  constructor(private httpClient: HttpClient) {
+    this.user$ = this.getUser();
+  }
+
+  private getUser() {
+    return this.httpClient.get('https://randomuser.me/api/');
+  }
 }
